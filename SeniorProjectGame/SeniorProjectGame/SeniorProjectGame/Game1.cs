e5f98d80c408ae2da6de1331509d5580fef6a4a7
Oliver Edward Barnum <thenextguy32@gmail.com>
2013-04-07 16:44:46 -0400

    Moving all classes except game1 to engine project
    In case we want to make a sequel, having all the code in the entity
    engine means we have more customization capabilities in the future.

--- SeniorProjectGame/SeniorProjectGame/SeniorProjectGame/Game1.cs
+++ SeniorProjectGame/SeniorProjectGame/SeniorProjectGame/Game1.cs
@@ -31,12 +31,11 @@ namespace SeniorProjectGame
         Random rand;
 
         Texture2D hexTexture;
-        Texture2D hexPiece;
+        Texture2D unitTexture;
 
         SpriteFont font;
 
-        Entity player;
-        InputAction mouseLeftClick, mouseRightClick, escapeAction;
+        InputAction mouseSingleLeftClick, mouseSingleRightClick, escapeAction;
 
         BoardComponent boardComp;
 
@@ -46,30 +45,28 @@ namespace SeniorProjectGame
             Content.RootDirectory = "Content";
 
             //1280x720
-            //graphics.PreferredBackBufferHeight = 720;
-            //graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 680;
+            graphics.PreferredBackBufferWidth = 1280;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            
 
             IsMouseVisible = true;
             LoadContent();
             CreateBoard();
 
+            //boardComp.CreateUnit(new Vector2(5, 5), hexPiece);
+
             rand = new Random();
             escapeAction = new InputAction(new Keys[] { Keys.Escape }, true);
-            mouseLeftClick = new InputAction(MouseButton.left, false);
-            mouseRightClick = new InputAction(MouseButton.right, false);
+            mouseSingleLeftClick = new InputAction(MouseButton.left, true);
+            mouseSingleRightClick = new InputAction(MouseButton.right, true);
 
+            #region stateInit
             State.screenState = State.ScreenState.SKIRMISH;
+            State.selectionState = State.SelectionState.NoSelection;
             State.dialoguePosition = 0;
             State.dialogueChoicePosition = 0;
             State.displayedDialogueMessage = "";
@@ -82,6 +79,7 @@ namespace SeniorProjectGame
             State.lastTimeDialogueChecked = 0;
             State.messageBegin = false;
             State.currentDialogueMessage = new List<string>();
+            #endregion
 
             Globals.font = Content.Load<SpriteFont>("font");
 
@@ -91,10 +89,9 @@ namespace SeniorProjectGame
         void CreateBoard()
         {
             Entity board = new Entity(0);
-            boardComp = new BoardComponent(board, hexTexture, font, new Vector2(20, 20));
+            boardComp = new BoardComponent(board, hexTexture, font, new Vector2(27, 12));
             board.AddComponent(boardComp);
             EntityManager.AddEntity(board);
-
         }
 
         /// <summary>
@@ -108,7 +105,7 @@ namespace SeniorProjectGame
 
             font = Content.Load<SpriteFont>("Debug");
             hexTexture = Content.Load<Texture2D>("hex");
-            hexPiece = Content.Load<Texture2D>("hexPiece");
+            unitTexture = Content.Load<Texture2D>("hexPiece");
 
         }
 
@@ -116,21 +113,52 @@ namespace SeniorProjectGame
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
         /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+
+            if (escapeAction.Evaluate())
+            {
+                this.Exit();
+            }
+
+            switch (State.screenState)
+            {
+                case State.ScreenState.MAIN_PAGE:
+                    break;
+                case State.ScreenState.SETTINGS_MENU:
+                    break;
+                case State.ScreenState.WORLD_MAP:
+                    break;
+                case State.ScreenState.SHOP:
+                    break;
+                case State.ScreenState.DIALOGUE:
+                    break;
+                case State.ScreenState.SKIRMISH:
+
+                    EntityManager.Update(gameTime);
+
+                    if (mouseSingleLeftClick.Evaluate())
+                    {
+                    }
+                    break;
+
+                //As of now, we will not be using the states below
+                case State.ScreenState.BATTLE_FORECAST:
+                    break;
+                case State.ScreenState.BATTLING:
+                    break;
+                case State.ScreenState.BATTLE_RESOLUTION:
+                    break;
+
+                
+
+                default:
+                    //This should nevr happen
+                    break;
+            }
 
             if (State.screenState == State.ScreenState.DIALOGUE)
             {
@@ -173,7 +201,7 @@ namespace SeniorProjectGame
                         }
                         else
                         {
-                            State.displayedDialogueMessage += curChar; 
+                            State.displayedDialogueMessage += curChar;
                             //  add chars blipping onto the screen
                         }
 
@@ -199,25 +227,67 @@ namespace SeniorProjectGame
             }
             else if (State.screenState == State.ScreenState.SKIRMISH)
             {
-                // skirmish map update
+
                 EntityManager.Update(gameTime);
 
-                if (escapeAction.Evaluate())
+                
+                if (mouseSingleLeftClick.Evaluate())
                 {
-                    this.Exit();
-                }
-                if (mouseLeftClick.Evaluate())
-                {
-                    HexComponent hexComp = boardComp.getCurrentHexAtMouse();
+                    HexComponent hexComp = boardComp.GetCurrentHexAtMouse();
                     Entity hexEntity = hexComp._parent;
+
+                    // PSEUDO-CODE OUTLINE BELOW. DO NOT ERASE! t.Jon
+
+                    
+                    if (hexComp.HasUnit() && State.selectionState == State.SelectionState.NoSelection) 
+                    {
+                        UnitComponent unit = hexComp.GetUnit();
+                        State.selectionState = State.SelectionState.SelectingUnit;
+
+                        State.originalHexClicked = hexComp;
+                    }
+
+
+
+                    //else if (State.selectionState == State.SelectionState.SelectingUnit)
+                    //{
+                    //    State.selectionState = State.SelectionState.SelectingOptionsForSkirmishUnits;
+                    //}
+                    //else if (State.selectionState == State.SelectionState.SelectingOptionsForSkirmishUnits) {
+
+                    //    if (ButtonPressed == "Back")
+                    //    {
+                    //        State.selectionState = State.SelectionState.NoSelection;
+                    //    }
+                    //    else if (OptionSelected == "Wait" || OptionSelected == "Item")
+                    //    {
+                    //        UnitComponent unit = State.originalHexClicked.GetUnit();
+                    //        State.originalHexClicked.RemoveUnit();
+
+                    //        hexComp.SetUnit(unit);
+
+                    //        State.SelectionState.NoSelection;
+                    //    }
+
+                    //}
+                    //*/
+
                     SpriteComponent sprite = hexEntity.getDrawable("SpriteComponent") as SpriteComponent;
 
                     sprite.setColor(Color.BurlyWood);
 
+                    //List<HexComponent> hexRing = boardComp.GetAllRings(3);
+                    //for (int p = 0; p < hexRing.Count; p++)
+                    //{
+                    //    Entity hexParent = hexRing[p]._parent;
+                    //    SpriteComponent spriteParent = hexParent.getDrawable("SpriteComponent") as SpriteComponent;
+                    //    spriteParent.setColor(Color.CadetBlue);
+                    //}
+
                 }
-                if (mouseRightClick.Evaluate())
+                if (mouseSingleRightClick.Evaluate())
                 {
-                    HexComponent hexComp = boardComp.getCurrentHexAtMouse();
+                    HexComponent hexComp = boardComp.GetCurrentHexAtMouse();
                     Entity hexEntity = hexComp._parent;
                     SpriteComponent sprite = hexEntity.getDrawable("SpriteComponent") as SpriteComponent;
 
@@ -237,7 +307,7 @@ namespace SeniorProjectGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
