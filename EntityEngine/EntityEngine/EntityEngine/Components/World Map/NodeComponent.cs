1c0cd661719dfb22ac2ee9a9e3796a6e4b5f5bed
Oliver Edward Barnum <thenextguy32@gmail.com>
2013-04-09 14:04:02 -0400

    Beginning of worldmap code
    The biggest thing is that node now needs docs that store levels since
    they store level naems as strings. Think about adding a Content Pipeline
    Extension for level.  "Tutorial.level" kind of thing.

---
+++ EntityEngine/EntityEngine/EntityEngine/Components/World Map/NodeComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityEngine.Components.Component_Parents;
+using Microsoft.Xna.Framework;
+using EntityEngine.Components.TileComponents;
+using EntityEngine.Components.Sprites;
+
+namespace EntityEngine.Components.World_Map
+{
+    public class NodeComponent : Component
+    {
+        NodeState nodeState;
+        public void SetNodeState(NodeState myState)
+        {
+            nodeState = myState;
+        }
+        public NodeState GetNodeState()
+        {
+            return nodeState;
+        }
+
+        string levelName;
+        public string GetLevelName()
+        {
+            return levelName;
+        }
+
+        public NodeComponent(string myLevelName)
+        {
+            this.name = "NodeComponent";
+            levelName = myLevelName;
+        }
+    }
+}
---
+++ EntityEngine/EntityEngine/EntityEngine/Components/World Map/WorldMapComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityEngine.Components.Component_Parents;
+using Microsoft.Xna.Framework;
+using EntityEngine.Components.TileComponents;
+using EntityEngine.Components.Sprites;
+
+namespace EntityEngine.Components.World_Map
+{
+    public class WorldMapComponent : Component
+    {
+
+        List<NodeComponent> nodeList = new List<NodeComponent>();
+        public void AddNode(NodeComponent myNode)
+        {
+            nodeList.Add(myNode);
+        }
+        
+
+        NodeComponent selectedNode;
+        public void SetSelectedNode(NodeComponent myNode)
+        {
+            selectedNode = myNode;
+        }
+
+        public WorldMapComponent()
+        {
+            this.name = "WorldMapComponent";
+        }
+
+        //Uses the specificed node
+        public void SelectNode(NodeComponent myNode)
+        {
+        }
+
+        //Uses the currently selected node
+        public void SelectNode()
+        {
+
+        }
+    }
+}
