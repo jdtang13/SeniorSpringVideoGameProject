aac74518b85327a1242afd3ac4aed537f045c066
Jon <jdtang13@gmail.com>
2013-05-02 16:28:22 -0400

    added buff duration

--- EntityEngine/EntityEngine/EntityEngine/Stat Attribute Classes/StatisticalBuff.cs
+++ EntityEngine/EntityEngine/EntityEngine/Stat Attribute Classes/StatisticalBuff.cs
@@ -15,6 +15,9 @@ namespace EntityEngine.Stat_Attribute_Classes
         int critBuff;
         int movementBuff;
 
+        int duration; //  a buff might be permanent (such as a buff on a weapon) or temporary
+                      //  (such as from a spell or song or potion). duration is measured in "turns"
+
         public StatisticalBuff(int healthBuff, int manaBuff, int avoidBuff, int critBuff, int movementBuff)
         {
             this.healthBuff = healthBuff;
@@ -22,7 +25,12 @@ namespace EntityEngine.Stat_Attribute_Classes
             this.avoidBuff = avoidBuff;
             this.critBuff = critBuff;
             this.movementBuff = movementBuff;
+
+            this.duration = 0; //  duration of zero = permanent buff
         }
+
+        public int Duration() { return duration; }
+        public void SetDuration(int duration) { this.duration = duration; }
 
         public int HealthBuff() { return healthBuff; }
         public int ManaBuff() { return manaBuff; }
